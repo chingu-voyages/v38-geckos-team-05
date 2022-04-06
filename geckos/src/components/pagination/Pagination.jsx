5c7750aca51e56c47c8ef129828276d9b99fb3bf
avicnotes <78015849+avicnotes@users.noreply.github.com>
2022-04-06 12:22:18 +0300

    create pagination component

---
+++ geckos/src/components/pagination/Pagination.jsx
@@ -0,0 +1,16 @@
+import {Container, Button} from "./style/pagination";
+
+const Pagination = ()=>{
+  
+    return (
+      <Container>
+        <Button>
+          Previous
+        </Button>
+        <Button>
+          Next
+        </Button>
+      </Container>
+    );
+}
+export default Pagination;
