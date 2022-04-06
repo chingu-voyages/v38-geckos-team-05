e80a0c1d3eddf954ca6cdbc274fad28bae64a0dd
avicnotes <78015849+avicnotes@users.noreply.github.com>
2022-04-06 12:20:32 +0300

    Create all user questions component

---
+++ geckos/src/components/question/AllUserQuestions.jsx
@@ -0,0 +1,43 @@
+const AllUserQuestions = ()=>{
+    return (
+      <>
+      <h1>All User's Questions</h1>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      </>
+    );
+}
+export default AllUserQuestions;
