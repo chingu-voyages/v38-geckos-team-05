f14ecd700fb997c6772cd7e802cc925a43d97bcc
avicnotes <78015849+avicnotes@users.noreply.github.com>
2022-04-06 12:20:16 +0300

    Create recent user questions component

---
+++ geckos/src/components/question/RecentUserQuestions.jsx
@@ -0,0 +1,29 @@
+const RecentUserQuestions = ()=>{
+    return (
+      <>
+      <h2>Recent User's Questions</h2>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+      </>
+    );
+}
+export default RecentUserQuestions;
