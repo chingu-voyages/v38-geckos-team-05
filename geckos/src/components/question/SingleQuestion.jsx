4896c3ddaaa9958a5133403c44c225bba452a44f
avicnotes <78015849+avicnotes@users.noreply.github.com>
2022-04-06 12:19:46 +0300

    Create single question component

---
+++ geckos/src/components/question/SingleQuestion.jsx
@@ -0,0 +1,12 @@
+const SingleQuestion = ()=>{
+    return (
+      <div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <a href="#">View Answer</a>
+        <hr/>
+      </div>
+    );
+}
+export default SingleQuestion;
