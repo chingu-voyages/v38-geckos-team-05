366c75602ee8aac010135f13b7c60b7af2e9282e
avicnotes <78015849+avicnotes@users.noreply.github.com>
2022-04-11 12:15:09 +0300

    Create ask page with styling

---
+++ geckos/src/pages/ask/Ask.jsx
@@ -0,0 +1,29 @@
+import {Main, Div, Container, H1, Img, Textarea, Input, Button} from "./style.js";
+import RecentUserQuestions from "../../components/question/RecentUserQuestions";
+import HomeButton from "../../components/homebutton/HomeButton";
+import {Link} from "react-router-dom";
+import UserProfilePic from "../../profile-images/user-milad-farhani.jpg";
+const Ask = ()=>{
+
+    return(
+     <Main>
+      <HomeButton/>
+      <H1>Ask Farhani</H1>
+      <Img src={UserProfilePic} alt="Farhani's Profile Picture"/>
+      <Container>      
+        <Div>
+          <form>
+            <Textarea rows="20" cols="50" placeholder="Type your question here"></Textarea>
+            <Input placeholder="Topic or Category"/>
+            <Button type="submit">Submit Question</Button>
+          </form>
+          <Link to="/user">
+            <Button>View All Farhani's Question</Button>
+          </Link>
+        </Div>
+        <RecentUserQuestions/>
+      </Container>  
+     </Main>
+    );
+}
+export default Ask;
