233ca79b42287bf5e0a2287f2dd51332bcded44f
avicnotes <78015849+avicnotes@users.noreply.github.com>
2022-05-06 19:31:14 +0300

    Display user's posts in Users page

--- geckos/src/components/recent/Recent.jsx
+++ geckos/src/components/recent/Recent.jsx
@@ -2,31 +2,17 @@ import {useState, useEffect } from 'react';
 import {Ul,List,H2,Contianer} from "./style/recent";
 import {Link} from "react-router-dom";
 
-function RoutedUsers( data ) {
-  const [usersdata, setData] = useState([]); 
 
-  useEffect(() => { 
-    const fetchData = () => {
-      fetch('http://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then((usersdata) => { setData(usersdata) })
-      };
-      fetchData();
-  },[]);
-  console.log(usersdata);
-}
 
 const Recent = ( data )=>{
-  let usersdata = RoutedUsers();
-
     return (
       <Contianer>
        <H2>Recent AMA'S</H2>
         <Ul>
           {data.topUsers.map(item => (
             <List>
-              <Link to="/user">{item.id}. <span>{item.name}</span></Link>
-              {/*<Link to={`/${item.id}`}>{item.id}. <span>{item.name}</span></Link>*/}
+              {/*<Link to="/user">{item.id}. <span>{item.name}</span></Link>*/}
+              <Link to={`/${item.id}`}>{item.id}. <span>{item.name}</span></Link>
             </List>
           ))}   
         </Ul>
--- geckos/src/pages/user/User.jsx
+++ geckos/src/pages/user/User.jsx
@@ -2,14 +2,87 @@ import {Main, Div, Container, H1} from "./style.js";
 import AllUserQuestions from "../../components/question/AllUserQuestions";
 import RecentUserQuestions from "../../components/question/RecentUserQuestions";
 import Pagination from "../../components/pagination/Pagination";
-import HomeButton from "../../components/homebutton/HomeButton"
+import HomeButton from "../../components/homebutton/HomeButton";
 
+import {Link, useParams} from "react-router-dom";
+import {useState, useEffect} from "react";
+
+function useFetch(url) {
+  const [loadingData, setLoadingData] = useState(false);
+  const [data, setData] = useState(null);
+  useEffect(() => { 
+    const fetchData = async() => {
+      try{
+        setLoadingData(true);
+        fetch(url)
+          .then(response => response.json())
+          .then((data) => { setData(data) })
+        setLoadingData(false);
+      } catch(error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  },[]);
+  return[loadingData, data];
+}
+
+function User() {
+  const [isDataLoading, data] = useFetch('http://jsonplaceholder.typicode.com/posts');
+
+  let params = useParams();
+
+  if (isDataLoading || !data){
+    return (<p>Data is loading...</p>)
+  }
+  else {
+    return (
+      <Main>
+        <HomeButton/>
+        <H1>Simple AMA: Influencer { params.userID }</H1>
+        <Container>      
+          <Div>
+            {(data.filter((post) => post.userId === parseInt(params.userID, 10) )).map((item) => (
+              <>
+                <p>{item.id}</p>
+                <p>{item.title}</p>
+                <p>{item.body}</p>
+              </>
+            ))}
+            <AllUserQuestions data= { (data.filter((post) => post.userId === parseInt(params.userID, 10) )) }/>
+            <Pagination/>
+          </Div>
+          <RecentUserQuestions/>
+        </Container>  
+        
+    
+        <h2> Single User: {params.userID} </h2>
+        {/*<p>Title:{userdata.title}</p>*/}
+      </Main>
+    )
+  }
+}
+
+{/*  useEffect(() => { 
+    const fetchData = () => {
+      fetch('http://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then((usersdata) => { setData(usersdata) })
+      };
+      fetchData();
+  },[]);
+  console.log(usersdata);
+}
 const User = ()=>{
+*/}
 
+{/*
+  let params = useParams();
+  
     return(
      <Main>
       <HomeButton/>
-      <H1>Simple AMA</H1>
+      <H1>Simple AMA: { params.userID }</H1>
       <Container>      
         <Div>
           <AllUserQuestions/>
@@ -20,4 +93,5 @@ const User = ()=>{
      </Main>
     );
 }
+*/}
 export default User;
